27a9e548af71519ca739790ef3eb7d6863c85631
Gary N. Bagnall <gbagnall8@gmail.com>
2018-12-16 22:45:37 +1000

    error testing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 $(window).load(() => {
-  //$.getJSON('https://garaxer.github.io/json', (gary) => {
+
     $(".picture").attr("src", gary.picture);
     $(".picture").click(() => window.open('./json/index.json'));
     $(".name").text(gary.name);
@@ -11,5 +11,16 @@ $(window).load(() => {
       $(".sites").append("<img class='button " + k + "' src='src/images/icon-" + k + ".png'>");
       $("." + k).click(() => window.open(v.base + v.user));
     });
-  });
-//})
+});
+$.getJSON('https://garaxer.github.io/json', (gary) => {
+  console.log(gary);
+});
+$.getJSON('http://garaxer.github.io/json', (gary) => {
+  console.log(gary);
+});
+$.getJSON('garaxer.github.io/json', (gary) => {
+  console.log(gary);
+});
+
+
+//jquery-1.12.4.js:10254 Mixed Content: The page at 'https://garaxer.github.io/' was loaded over HTTPS, but requested an insecure XMLHttpRequest endpoint 'http://garaxer.github.io/json'. This request has been blocked; the content must be served over HTTPS.
